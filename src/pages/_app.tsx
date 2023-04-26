3c31432e379a83590b1596e9d7d197888abb4c1e
oukalatte <oukalatte@gmail.com>
2023-04-26 09:25:30 +0800

    button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,24 +3,10 @@ import { api } from "@/utils/api";
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import { useEffect, useRef } from "react";
 import Head from "next/head";
-import { MainScene } from "@/gl/parts/MainScene";
 import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  const three = useRef<MainScene>();
-  const canvas = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    if (three.current) return;
-    if (!canvas.current) return;
-
-    three.current = new MainScene({
-      el: canvas.current,
-    });
-  }, []);
-
   return (
     <main>
       <Head>
